476bde82de91ca309dccc057520d15e63d6e6bfc
Sean Clarke <me@seanland.ca>
2020-01-14 10:52:20 -0500

    updated styles, visit images have been added to sites

--- database/fill-visits.js
+++ database/fill-visits.js
@@ -2,9 +2,9 @@ var con = require('./db');
 
 sql = "INSERT INTO visits (date, img, comment, site_id) VALUES ?";
 val = [
-  ['10-15-2019','https://seanland.ca', 'This is a comment about the place I went, or some details max 1000 characters.  They are not supposed to be super long - but you can write a bit.' ,'203'],
-  ['10-17-2019','https://seanland.ca', 'This is a comment about the place I went, or some details max 1000 characters.  They are not supposed to be super long - but you can write a bit.' ,'169'],
-  ['01-17-2020','https://seanland.ca', 'This is a comment about the place I went, or some details max 1000 characters.  They are not supposed to be super long - but you can write a bit.' ,'520'],
+  ['08-06-2018','/images/20180806_141233.png', 'The one picture featuring a guest appearance.  Please welcome Diana!' ,'1475'],
+  ['10-17-2016','/images/20161017_111122.png', 'No Comment.' ,'917'],
+  ['10-17-2016','/images/20161017_150044.png', 'No Comment.' ,'918'],
   ['11-23-2019','https://seanland.ca', 'This is a comment about the place I went, or some details max 1000 characters.  They are not supposed to be super long - but you can write a bit.' ,'520']
 ];
 
